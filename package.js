1a1087581d789fe6087187dafa7373d68659a148
Ramazan <126978020+ramazannwashere@users.noreply.github.com>
2023-08-12 18:34:55 +0300

    package.js

--- package.js
+++ package.js
@@ -1,18 +1,1 @@
-{
-  "isim" : " Ramo " ,
-  "sürüm" : " 1.0.0 " ,
-  "açıklama" : " " ,
-  "ana" : " tokyo.js " ,
-  "komut dosyaları" : {
-    "start" : " tokyo.js düğümü "
-  },
-  "motorlar" : {
-    "düğüm" : " 12.x "
-  },
-  "yazar" : " " ,
-  "lisans" : " ISC " ,
-  "bağımlılıklar" : {
-    "aoi.js" : " ^3.0.0 " ,
-    "yt-arama" : " ^2.8.0 "
-  }
-}
+gg
